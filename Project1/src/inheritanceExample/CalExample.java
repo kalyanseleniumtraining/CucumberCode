1e82498c6f6eddf4d59e2db9f51016ec46ccd3d5
S@f' <S@f'@Sf-PC>
2019-10-14 09:18:57 +0530

    14th Oct Code

---
+++ Project1/src/inheritanceExample/CalExample.java
@@ -0,0 +1,54 @@
+package inheritanceExample;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CalExample {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.apsrtconline.in/oprs-web/guest/home.do?h=1");
+		
+		driver.findElement(By.id("txtJourneyDate")).click();
+		
+		List<WebElement> rows=driver.findElements(By.xpath("(.//table[@class='ui-datepicker-calendar'])[1]/tbody/tr"));
+		
+		
+		OutLoop: for(int i=1;i<=rows.size();i++)
+		{
+			List<WebElement> cols=driver.findElements(By.xpath("(.//table[@class='ui-datepicker-calendar'])[1]/tbody/tr["+i+"]/td"));
+			
+			
+			InLoop: for(int j=1;j<=cols.size();j++)
+			{
+				WebElement ele=driver.findElement(By.xpath("(.//table[@class='ui-datepicker-calendar'])[1]/tbody/tr["+i+"]/td["+j+"]"));
+				String date=ele.getText();
+				if(date.equals("30"))
+				{
+					ele.click();
+					break OutLoop;
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
